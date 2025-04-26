d02a45f0e432d032534c1c0058712d73e7c7b07d
Adrian Obregon <adrianlolobre@gmail.com>
2025-04-26 04:56:21 -0600

    v2.0

--- js/main.js
+++ js/main.js
@@ -10,32 +10,31 @@ cerrar.addEventListener('click', () => {
     nav.classList.remove("nav-visible");
 })
 
-
-const darkMode = document.querySelector("#dark-mode");
-const lightMode = document.querySelector("#light-mode");
+const modoOscuro = document.querySelector("#modo-oscuro");
+const modoClaro = document.querySelector("#modo-claro");
 const body = document.querySelector("#body")
 
-function darkModeActivo() {
+function modoOscuroActivo() {
     body.style.background = '#282928';
     body.style.color = 'white';
-    darkMode.style.display = 'none';
-    lightMode.style.display = 'block';
-    localStorage.setItem('darkMode', 'activo');
+    modoOscuro.style.display = 'none';
+    modoClaro.style.display = 'block';
+    localStorage.setItem('modoOscuro', 'activo');
 };
 
-function lightModeActivo() {
+function modoClaroActivo() {
     body.style.background = 'white';
     body.style.color = 'black';
-    lightMode.style.display = 'none';
-    darkMode.style.display = 'block';
-    localStorage.setItem('darkMode', 'desactivo');
+    modoClaro.style.display = 'none';
+    modoOscuro.style.display = 'block';
+    localStorage.setItem('modoOscuro', 'desactivo');
 };
 
-darkMode.addEventListener('click', darkModeActivo);
-lightMode.addEventListener('click', lightModeActivo);
+modoOscuro.addEventListener('click', modoOscuroActivo);
+modoClaro.addEventListener('click', modoClaroActivo);
 
-if(localStorage.getItem('darkMode') === 'activo') {
-    darkModeActivo();
+if(localStorage.getItem('modoOscuro') === 'activo') {
+    modoOscuroActivo();
 } else {
-    lightModeActivo();
+    modoClaroActivo();
 }
